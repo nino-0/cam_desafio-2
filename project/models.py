a25144a0e92c2bf51d2b966e04231241f7f14d83
nino-0 <manoelvitor@acad.ifma.edu.br>
2023-04-22 09:50:36 -0300

    Finalizando o relacionamento da maneira correta manyToMany project has employees working

--- project/models.py
+++ project/models.py
@@ -7,12 +7,6 @@ import datetime
 class Supervisor(models.Model):    
     employee_id = models.ForeignKey(Employee,on_delete=models.SET_NULL,null=True)
     workload = models.FloatField(default=0)
-
-class EmployeeWorkProject(models.Model): 
-    employee = models.ForeignKey(Employee,on_delete=models.CASCADE)
-    workload = models.FloatField(default=0)
-    class Meta:
-      unique_together = ('employee',)
 class Project(models.Model):
   department_id = models.ForeignKey(Deparment, on_delete=models.CASCADE)
   name = models.CharField(max_length=150)
@@ -21,9 +15,14 @@ class Project(models.Model):
   estimated_date = models.DateField(null=True)
   last_measurement = models.DateField(default=datetime.date.today)
   supervisor = models.OneToOneField(Supervisor, on_delete=models.CASCADE)
-  work_group = models.ManyToManyField(EmployeeWorkProject)
+  work_group = models.ManyToManyField(Employee,through='EmployeeWorkProject')
   class Meta:
     unique_together = ('name', 'department_id',)
   def __str__(self) -> str:
       return self.name
-  
+class EmployeeWorkProject(models.Model):
+    project = models.ForeignKey(Project,on_delete=models.CASCADE) 
+    employee = models.ForeignKey(Employee,on_delete=models.CASCADE)
+    workload = models.FloatField(default=0)
+    class Meta:
+      unique_together = ('project', 'employee',)
